a8a127c8abbf64bbb421d98677d61792544ea541
yyounghoon <yhyoon@huray.net>
2022-05-19 17:10:05 +0900

    version 1.0.1

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: "module",
+    },
+    rules: {
+        eqeqeq: "error"
+    }
+}
